0050e13a29ee4ce63566a69599f5ee301dabef7e
taekyungss <wendy1080@naver.com>
2024-07-29 13:18:41 +0000

    chore : subject 2 model train

--- code/config.py
+++ code/config.py
@@ -5,7 +5,79 @@ class Config_MAE_fMRI: # back compatibility
     pass
 class Config_MBM_finetune: # back compatibility
     pass 
+class Config_MBM_fMRI(Config_MAE_fMRI):
+    # configs for fmri_pretrain.py
+    def __init__(self):
+    # --------------------------------------------
+    # MAE for fMRI
+        # Training Parameters
+        self.lr = 2.5e-4
+        self.min_lr = 0.
+        self.weight_decay = 0.05
+        self.num_epoch = 500
+        self.warmup_epochs = 40
+        self.batch_size = 100
+        self.clip_grad = 0.8
+        
+        # Model Parameters
+        self.mask_ratio = 0.75
+        self.patch_size = 16
+        self.embed_dim = 1024 # has to be a multiple of num_heads
+        self.decoder_embed_dim = 512
+        self.depth = 24
+        self.num_heads = 16
+        self.decoder_num_heads = 16
+        self.mlp_ratio = 1.0
 
+        # Project setting
+        self.root_path = '.'
+        self.output_path = self.root_path
+        self.seed = 2022
+        self.roi = 'VC'
+        self.aug_times = 1
+        self.num_sub_limit = None
+        self.include_hcp = True
+        self.include_kam = True
+        self.accum_iter = 1
+
+        self.use_nature_img_loss = False
+        self.img_recon_weight = 0.5
+        self.focus_range = None # [0, 1500] # None to disable it
+        self.focus_rate = 0.6
+
+        # distributed training
+        self.local_rank = 0
+
+class Config_MBM_finetune(Config_MBM_finetune):
+    def __init__(self):
+        
+        # Project setting
+        self.root_path = '.'
+        self.output_path = self.root_path
+        self.kam_path = os.path.join(self.root_path, 'data/Kamitani/npz')
+        self.bold5000_path = os.path.join(self.root_path, 'data/BOLD5000')
+        self.dataset = 'GOD' # GOD  or BOLD5000
+        self.pretrain_mbm_path = os.path.join(self.root_path, f'pretrains/{self.dataset}/fmri_encoder.pth') 
+
+        self.include_nonavg_test = True
+        self.kam_subs = ['sbj_3']
+        self.bold5000_subs = ['CSI1']
+
+        # Training Parameters
+        self.lr = 5.3e-5
+        self.weight_decay = 0.05
+        self.num_epoch = 15
+        self.batch_size = 16 if self.dataset == 'GOD' else 4 
+        self.mask_ratio = 0.75 
+        self.accum_iter = 1
+        self.clip_grad = 0.8
+        self.warmup_epochs = 2
+        self.min_lr = 0.
+
+        # distributed training
+        self.local_rank = 0
+
+        
 
 # 수정한 코드
 class Config_MBM_EEG(Config_MAE_fMRI):
@@ -22,11 +94,12 @@ class Config_MBM_EEG(Config_MAE_fMRI):
         self.weight_decay = 0.15
         self.num_epoch = 500
         self.warmup_epochs = 40
-        self.batch_size = 32
+        self.batch_size = 64
         self.clip_grad = 0.8
-        
+        self.num_classes = 40
+
         # Model Parameters
-        self.mask_ratio = 0.35
+        self.mask_ratio = 0.75
         self.patch_size = 4 #  1
         self.embed_dim = 1024 #256 # has to be a multiple of num_heads -> 원래 dimension은 128차원 -> num_heads =8 -> 128*8 = 1024
         self.decoder_embed_dim = 512 #128
--- code/visualize_tsne.py
+++ code/visualize_tsne.py
@@ -1,12 +1,12 @@
-from sc_mbm.mae_for_eeg import MAEforEEG
+from sc_mbm.mae_for_eeg_2 import MAEforEEG
 from config import Config_MBM_EEG
-from dataset import eeg_pretrain_dataset
 from stageA1_eeg_pretrain import fmri_transform
 from torch.utils.data import DataLoader
 from matplotlib import pyplot as plt
 from matplotlib.offsetbox import OffsetImage, AnnotationBbox, TextArea
 from sklearn.manifold import TSNE
 import numpy as np
+from dataset import EEGDataset_subject
 
 import torch
 import torch.nn as nn
@@ -15,26 +15,27 @@ config = Config_MBM_EEG()
 local_rank = config.local_rank
 device = torch.device(f'cuda:{local_rank}')
 
-valid_dataset = eeg_pretrain_dataset(path='/Data/summer24/DreamDiffusion/datasets/eegdata/train/eeg', roi=config.roi, patch_size=config.patch_size,
-                transform=fmri_transform, aug_times=config.aug_times, num_sub_limit=config.num_sub_limit, 
-            include_kam=config.include_kam, include_hcp=config.include_hcp)
+train_dataset = EEGDataset_subject(eeg_signals_path="/Data/summer24/DreamDiffusion/datasets/eegdata_2subject/train.pth", mode = "train")
+train_dataloader_eeg = DataLoader(train_dataset, batch_size=config.batch_size,
+            shuffle=False, pin_memory=True)
 
+valid_dataset = EEGDataset_subject(eeg_signals_path='/Data/summer24/DreamDiffusion/datasets/eegdata_2subject/val.pth', mode = "val")
 valid_dataloader_eeg = DataLoader(valid_dataset, batch_size=config.batch_size,
             shuffle=False, pin_memory=True)
 
 
-model = MAEforEEG(time_len=1024, patch_size=config.patch_size, embed_dim=config.embed_dim,
+model = MAEforEEG(time_len=valid_dataset.data_len, patch_size=config.patch_size, embed_dim=config.embed_dim,
                     decoder_embed_dim=config.decoder_embed_dim, depth=config.depth, 
                     num_heads=config.num_heads, decoder_num_heads=config.decoder_num_heads, mlp_ratio=config.mlp_ratio,
                     focus_range=config.focus_range, focus_rate=config.focus_rate, 
-                    img_recon_weight=config.img_recon_weight, use_nature_img_loss=config.use_nature_img_loss)
+                    img_recon_weight=config.img_recon_weight, use_nature_img_loss=config.use_nature_img_loss, num_classes=config.num_classes)   
 
-checkpoint = torch.load("/Data/summer24/DreamDiffusion/DreamDiffuion/results/eeg_pretrain/11-07-2024-07-07-28/checkpoints/checkpoint.pth", map_location='cpu')  # Modify the path to your checkpoint
+checkpoint = torch.load("/Data/summer24/DreamDiffusion/DreamDiffuion/results/eeg_pretrain/29-07-2024-09-13-33/checkpoints/checkpoint.pth", map_location='cpu')  # Modify the path to your checkpoint
 model.load_state_dict(checkpoint['model'], strict=False)
 model.eval()
 all_latents = []
 import pandas as pd
-df = pd.read_csv("/Data/summer24/DreamDiffusion/datasets/eegdata/train/subject.csv",index_col=0)
+df = pd.read_csv("/Data/summer24/DreamDiffusion/datasets/eegdata_subject2_2/val/label.csv",index_col=0)
 
 with torch.no_grad():
     all_latents = []
@@ -46,11 +47,12 @@ with torch.no_grad():
         latent = latent.view(batch_size, -1) 
         all_latents.append(latent.cpu())
 
+# Concatenate all latent representations
 all_latents = np.concatenate(all_latents, axis=0)
 all_labels = df.values
 
 
-tsne = TSNE(n_components=2,perplexity=40)
+tsne = TSNE(n_components=2)
 latent_tsne = tsne.fit_transform(all_latents)
 
 plt.figure(figsize=(10, 8))
@@ -60,4 +62,4 @@ plt.title('2D t-SNE Visualization of Latent Representations')
 plt.scatter(latent_tsne[:, 0], latent_tsne[:, 1],c=all_labels, alpha=0.5)
 plt.colorbar()
 plt.show()
-plt.savefig('/Data/summer24/DreamDiffusion/result/p40_11-07_checkpoint_tsne_train_subject.png')
+plt.savefig('/Data/summer24/DreamDiffusion/tsne_result/29-07_tsne_img_label_train.png')
